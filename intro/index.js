c697644313e1c0e4aa3f5b971d0ae2555c865e8a
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-12-26 13:21:12 -0300

    challenge n4

---
+++ intro/index.js
@@ -0,0 +1,15 @@
+console.log("Hello world");
+
+const a = 1;
+let b = 1;
+var c = 1;
+
+const sum = a + b + c;
+const subtracion = a - b;
+const multiplication = a * b * c;
+const remainder = a % c;
+
+console.log(sum);
+console.log(subtracion);
+console.log(multiplication);
+console.log(remainder);
